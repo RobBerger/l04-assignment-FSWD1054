731c1e15cd9f7641b6040c5b557366a390c5b469
berger <robertberger.dev@gmail.com>
2023-02-16 11:33:44 -0500

    setup App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import { faker } from '@faker-js/faker';
 
 class App extends React.Component {
+
+constructor(props) {
+  super(props);
+  this.state = { vehicles: this.createCar(), selected: null };
+  this.selectedCar = this.selectedCar.bind(this);
+}
+
+createCar() {
+  let vehicles = [];
+  for (let i = 0; i < 10; i++) {
+    vehicles.push({
+      manufacturer: faker.vehicle.manufacturer(),
+      model: faker.vehicle.model(),
+      type: faker.vehicle.type(),
+      fuel: faker.vehicle.fuel(),
+      vin: faker.vehicle.vin(),
+      color: faker.vehicle.color()
+    })
+  }
+  return vehicles;
+}
+
+selectedCar(vehicle) {
+  this.setState({ selected: vehicle});
+}
+
   render() {
     return (
       <>hi</>
